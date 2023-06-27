c475d466c70019e4f3c8c75a619e976df234ec82
Souradeep Ghosh <116977697+unicornjohn@users.noreply.github.com>
2023-06-27 22:18:50 +0530

    Create Union-of-Two-Linked-Lists.cpp

---
+++ Union-of-Two-Linked-Lists.cpp
@@ -0,0 +1,27 @@
+
+class Solution
+{
+    public:
+    struct Node* makeUnion(struct Node* head1, struct Node* head2)
+    {
+        map<int,int>mp;
+        Node* p1=head1,*p2=head1;
+        while(p1->next!=NULL){
+            mp[p1->data]++;
+            p1=p1->next;
+        }
+        p1->next=head2;
+        while(p1!=NULL){
+            mp[p1->data]++;
+            p1=p1->next;
+        }
+        int cnt=mp.size();
+        for(auto it:mp){
+            p2->data=it.first;
+            cnt--;
+            if(cnt)p2=p2->next;
+        }
+        p2->next=NULL;
+        return head1;
+    }
+};
