1669dc814761a3b4e3f47f61cb90a3c93c2c6e7b
Souradeep Ghosh <116977697+unicornjohn@users.noreply.github.com>
2023-07-25 23:35:08 +0530

    Create  Level order traversal in spiral form.cpp

---
+++ Level order traversal in spiral form.cpp
@@ -0,0 +1,31 @@
+vector<int> findSpiral(Node *root)
+{
+    int rev = 1;
+    vector<int>ans;
+    deque<Node*>q;
+    q.push_back(root);
+    while(!q.empty()){
+        int sz = q.size();
+        while(sz--){
+            Node *curr;
+            if(rev){
+                curr = q.back(); q.pop_back();
+                if(curr -> right)
+                    q.push_front(curr -> right);
+                if(curr -> left)
+                    q.push_front(curr -> left);
+            }
+            else{
+                curr = q.front(); q.pop_front();
+                if(curr -> left)
+                    q.push_back(curr -> left);
+                if(curr -> right)
+                    q.push_back(curr -> right);
+                
+            }
+            ans.push_back(curr -> data);
+        }
+        rev = !rev;
+    }
+    return ans;
+}
