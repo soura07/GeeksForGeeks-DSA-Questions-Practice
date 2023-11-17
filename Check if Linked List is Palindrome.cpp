dfe4fa5d95674d99443bb7c91dd92e2e9652dd7e
Souradeep Ghosh <116977697+soura07@users.noreply.github.com>
2023-11-17 22:56:39 +0530

    Create Check if Linked List is Palindrome.cpp

---
+++ Check if Linked List is Palindrome.cpp
@@ -0,0 +1,32 @@
+#include<vector>
+class Solution{
+  private: 
+    bool checkPalindrome(vector<int> arr){
+        int n = arr.size();
+        int s = 0;
+        int e = n-1;
+        
+        while(s<=e){
+            if(arr[s] != arr[e]){
+                return 0;
+            }
+            s++;
+            e--;
+        }
+        return 1;
+    }
+  public:
+    //Function to check whether the list is palindrome.
+    bool isPalindrome(Node *head)
+    {
+            vector<int> arr;
+            
+            Node* temp = head;
+            
+            while(temp != NULL){
+                arr.push_back(temp->data);
+                temp = temp -> next;
+            }
+            return checkPalindrome(arr);
+    }
+};
